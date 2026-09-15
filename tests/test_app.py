88f1d67335ce2493f33c1dee04093aca53f74336
Aasim <your-email@gmail.com>
2026-09-15 06:39:34 -0700

    Test scenario 1 assertion failure

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,5 +4,5 @@ from app.app import app
 def test_home():
     client = app.test_client()
     response = client.get("/")
-    assert response.status_code == 200
+    assert response.status_code == 500
     assert b"AI CI/CD Failure Analyzer is running!" in response.data
